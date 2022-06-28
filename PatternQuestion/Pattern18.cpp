da84615bbe95bb1d6f23a2edb894d8867e38cf8a
shahbaz-cse <cseshahbaz@gmail.com>
2022-06-28 16:23:38 +0530

    Solved Pattern 18

--- PatternQuestion/Pattern18.cpp
+++ PatternQuestion/Pattern18.cpp
@@ -12,6 +12,18 @@ int main(){
     int row, col, tempRow, count;
     cout<<"Print the no. of rows: ";
     cin>>row;
-    
-    
+    tempRow=1;
+    char alpha;
+    while (tempRow<=row)
+    {
+        col=1;
+        alpha='A'+row-tempRow;
+        while(col<=tempRow){
+            cout<<alpha;
+            alpha++;
+            col++;
+        }
+        cout<<"\n";
+        tempRow++;
+    }
 }
