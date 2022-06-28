266b98d4899d39f6cc0bc808da8f8df8e10fd2f0
shahbaz-cse <cseshahbaz@gmail.com>
2022-06-28 12:30:09 +0530

    Solved Pattern 8

--- PatternQuestion/Pattern8.cpp
+++ PatternQuestion/Pattern8.cpp
@@ -12,6 +12,19 @@ int main(){
     int row, col, tempRow, count;
     cout<<"Print the no. of rows: ";
     cin>>row;
+    tempRow=1;
+    count=1;
+    while (tempRow<=row)
+    {
+        col=1;
+        while(col<=tempRow){
+            cout<<count<<" ";
+            count++;
+            col++;
+        }
+        cout<<"\n";
+        tempRow++;
+    }
     
     
 }
