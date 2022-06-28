d19e70b8d17a19d222b0057fb113d0c9873a188c
shahbaz-cse <cseshahbaz@gmail.com>
2022-06-28 15:57:03 +0530

    Solved Pattern 17

--- PatternQuestion/Pattern17.cpp
+++ PatternQuestion/Pattern17.cpp
@@ -9,9 +9,20 @@ Print the pattern given below:
 */
 
 int main(){
-    int row, col, tempRow, count;
+    int row, col, tempRow;
     cout<<"Print the no. of rows: ";
     cin>>row;
-    
-    
+    tempRow=1;
+    char alpha;
+    while (tempRow<=row)
+    {
+        col=1;
+        while(col<=tempRow){
+            alpha='A'+tempRow+col-2;
+            cout<<alpha;
+            col++;
+        }
+        cout<<"\n";
+        tempRow++;
+    }
 }
