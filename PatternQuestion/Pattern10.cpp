179fb29a8d6ed3134a170cba2b989f85fdd8a239
shahbaz-cse <cseshahbaz@gmail.com>
2022-06-28 13:14:39 +0530

    Solved Pattern 10

--- PatternQuestion/Pattern10.cpp
+++ PatternQuestion/Pattern10.cpp
@@ -12,6 +12,18 @@ int main(){
     int row, col, tempRow, count;
     cout<<"Print the no. of rows: ";
     cin>>row;
+    tempRow=1;
+    
+    while (tempRow<=row)
+    {
+        col=1;    
+        while(col<=tempRow){
+            cout<<tempRow-col+1;
+            col++;
+        }
+        cout<<"\n";
+        tempRow++;
+    }
     
     
 }
