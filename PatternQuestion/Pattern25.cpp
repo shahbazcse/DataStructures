debe73f9786081df2abc08dd64cd37e1e671487f
shahbaz-cse <cseshahbaz@gmail.com>
2022-06-29 19:32:23 +0530

    Solved Pattern 25

--- PatternQuestion/Pattern25.cpp
+++ PatternQuestion/Pattern25.cpp
@@ -12,6 +12,22 @@ int main(){
     int row, col, tempRow, count;
     cout<<"Print the no. of rows: ";
     cin>>row;
-    
-    
+    tempRow=1;
+    count=1;
+    while (tempRow<=row)
+    {
+      int space=row-tempRow;
+      while(space>0){
+        cout<<" ";
+        space--;
+      }
+      col=1;
+      while(col<=tempRow){
+        cout<<count;
+        count++;
+        col++;
+      }
+      cout<<"\n";
+      tempRow++;
+    }
 }
