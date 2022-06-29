d0819cf6ecb771304264a2dd1b54d4d78c873440
shahbaz-cse <cseshahbaz@gmail.com>
2022-06-29 15:31:18 +0530

    Solved Pattern 23

--- PatternQuestion/Pattern23.cpp
+++ PatternQuestion/Pattern23.cpp
@@ -9,9 +9,24 @@ Print the pattern given below:
 */
 
 int main(){
-    int row, col, tempRow, count;
+    int row, col, tempRow;
     cout<<"Print the no. of rows: ";
     cin>>row;
-    
+    tempRow=1;
+    while (tempRow<=row)
+    {
+        int space=row-tempRow;
+        while(space>0){
+            cout<<" ";
+            space--;
+        }
+        col=1;
+        while(col<=tempRow){
+            cout<<tempRow;
+            col++;
+        }
+        cout<<"\n";
+        tempRow++;
+    }
     
 }
