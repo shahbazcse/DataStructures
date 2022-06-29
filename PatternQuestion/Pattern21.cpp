604d17612e016acb8dbd52460a310b177d39dcd3
shahbaz-cse <cseshahbaz@gmail.com>
2022-06-29 14:24:37 +0530

    Solved Pattern 21

--- PatternQuestion/Pattern21.cpp
+++ PatternQuestion/Pattern21.cpp
@@ -9,9 +9,27 @@ Print the pattern given below:
 */
 
 int main(){
-    int row, col, tempRow, count;
+    int row, col, tempRow;
     cout<<"Print the no. of rows: ";
     cin>>row;
+    tempRow=1;
+    while (tempRow<=row)
+    {
+        int space = 1;
+        while (space<=tempRow-1)
+        {
+            cout<<" ";
+            space++;
+        }
+        col=1;
+        while(col<=row-tempRow+1){
+            cout<<"*";
+            col++;
+        }
+        
+        cout<<"\n";
+        tempRow++;
+    }
     
     
 }
