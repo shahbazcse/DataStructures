32ba7466d3e708f43007e02ccbff878f3bad7fbc
shahbaz-cse <cseshahbaz@gmail.com>
2022-06-30 21:27:36 +0530

    Solved B2D and D2B Conversion

---
+++ BinaryToDecimal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=0;
+    int ans=0;
+    while (n!=0)
+    {
+        int bit=n%10; // bit is in the form of digit, hence the formula
+        if(bit==1){
+            ans=ans+pow(2,i);
+        }
+        n=n/10;
+        i++;
+    }
+    cout<<ans<<endl;
+}
