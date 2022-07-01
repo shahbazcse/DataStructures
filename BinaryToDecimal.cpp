9a12522d3e596d0236acf9c2302b26d7b158b762
shahbaz-cse <cseshahbaz@gmail.com>
2022-07-01 23:12:49 +0530

    Solved B2D Conversion

--- BinaryToDecimal.cpp
+++ BinaryToDecimal.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 int main(){
     int n;
+    cout<<"Enter Binary Number: ";
     cin>>n;
     int i=0;
     int ans=0;
     while (n!=0)
     {
-        int bit=n%10; // bit is in the form of digit, hence the formula
+        int bit=n%10; // To get bit
         if(bit==1){
-            ans=ans+pow(2,i);
+            ans=ans+pow(2,i); // To convert Binary form into Decimal form
         }
-        n=n/10;
+        n=n/10; // To move to the next digit
         i++;
     }
-    cout<<ans<<endl;
+    cout<<"Decimal Number: "<<ans<<endl;
 }
