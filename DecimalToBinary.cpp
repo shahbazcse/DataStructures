1c79279608c7a804a5886d5489fa4e775a3d7107
shahbaz-cse <cseshahbaz@gmail.com>
2022-07-01 23:14:49 +0530

    Solved D2B Conversion

--- DecimalToBinary.cpp
+++ DecimalToBinary.cpp
@@ -5,14 +5,15 @@ using namespace std;
 
 int main(){
     int n;
+    cout<<"Enter Decimal Number: ";
     cin>>n;
     int ans=0;
     int i=0;
     while(n!=0){
-        int bit=n&1;
-        ans=(bit*pow(10,i))+ans;
-        n=n>>1;
+        int bit=n&1; // To get bit
+        ans=(bit*pow(10,i))+ans; // To reverse the number
+        n=n>>1; // To move to next digit
         i++;
     }
-    cout<<ans;
+    cout<<"Binary Number: "<<ans<<endl;
 }
