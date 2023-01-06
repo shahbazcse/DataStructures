19747fcc22c83d9412fe44093376f16db3afd21d
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-07 00:56:07 +0530

    Solved Array Problems

--- Arrays/allPairsOfSum.cpp
+++ Arrays/allPairsOfSum.cpp
@@ -1,5 +1,6 @@
 // Link: https://practice.geeksforgeeks.org/problems/find-all-pairs-whose-sum-is-x5808/1
 
+// Approach 1: Brute Force [TC: N*M]
 class Solution{
     public:
     vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
@@ -11,6 +12,34 @@ class Solution{
             }
         }
         sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+// Approach 2: Binary Search [TC: NLogN]
+
+class Solution{
+    public:
+    // Binary Search
+    bool isPair(int low, int high, int key, int B[]){
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(B[mid]==key) return true;
+            else if(B[mid]<key) low=mid+1;
+                 else high=mid-1;
+        }
+        return false;
+    }
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        sort(A,A+N);
+        sort(B,B+M);
+        
+        vector<pair<int,int>> ans;
+        
+        for(int i=0; i<N; i++){
+            if(isPair(0,M-1,X-A[i],B)) ans.push_back({A[i],X-A[i]});
+        }
         return ans;
     }
 };
---
+++ Arrays/spiralMatrix.cpp
@@ -0,0 +1,41 @@
+// Link: https://practice.geeksforgeeks.org/problems/cd61add036272faa69c6814e34aa7007d5a25aa6/1
+
+// Approach: [TC: O(N*M)]
+
+class Solution{
+	public:
+	int findK(vector<vector<int>> &a, int n, int m, int k)
+    {
+        vector<int> ans;
+        
+        int top=0;
+        int bottom=n-1;
+        int left=0;
+        int right=m-1;
+        
+        while(top<=bottom && left<=right){
+            for(int i=left; i<=right; i++){
+                ans.push_back(a[top][i]);
+            }
+            top++;
+            for(int i=top; i<=bottom; i++){
+                ans.push_back(a[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right; i>=left; i--){
+                    ans.push_back(a[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom; i>=top; i--){
+                    ans.push_back(a[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans[k-1];
+    }
+
+};
