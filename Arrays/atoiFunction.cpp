21192341d9c464f8888e9f8ef6d1be0dda1320cc
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-09 01:17:57 +0530

    Solved Array Problems

--- Arrays/atoiFunction.cpp
+++ Arrays/atoiFunction.cpp
@@ -20,3 +20,34 @@ class Solution{
         return sign*sum;
     }
 };
+
+// Approach 2: Using Recursion [TC: O(str.length)]
+
+class Solution{
+  public:
+    int atoiRec(int &i, int &sum, string &str){
+        if(i==str.length()) return true;
+        
+        if(!(str[i]-'0'>=0 && str[i]-'0'<=9)) return false;
+
+        sum=sum*10+str[i]-'0';
+        i++;
+        return atoiRec(i,sum,str);
+    }
+    /*You are required to complete this method */
+    int atoi(string str) {
+        int sum=0;
+        int sign=1;
+        bool flag;
+        int i=0;
+        
+        if(str[0]=='-'){
+            sign=-1;
+            i++;
+            flag=atoiRec(i,sum,str);
+        }
+        else flag=atoiRec(i,sum,str);
+        
+        return (flag) ? sign*sum : -1;
+    }
+};
