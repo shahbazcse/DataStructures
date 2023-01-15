4cf6bb455388197509b3d458199457d83837abb7
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-15 19:31:47 +0530

    Solved 2P Problems

---
+++ TwoPointers/countTriplets.cpp
@@ -0,0 +1,26 @@
+// Link: https://practice.geeksforgeeks.org/problems/count-the-triplets4615/1
+
+// Approach: Sort + Reverse + 2 Pointer + Binary Search
+
+class Solution{
+public:	
+	
+	int countTriplet(int arr[], int n)
+	{
+	    int cnt=0;
+	    sort(arr,arr+n); // Sort
+	    reverse(arr,arr+n); // Reverse
+	    for(int i=0; i<n; i++){
+            // 2 Pointer - s,e
+	        int s=i+1;
+	        int e=n-1;
+            // Binary Search
+	        while(s<e){
+	            if(arr[s]+arr[e]==arr[i]) cnt++;
+	            if(arr[s]+arr[e]<arr[i]) e--;
+	            else s++;
+	        }
+	    }
+	    return cnt;
+	}
+};
