ac9fc8f537f093380aaed9ac651474f7e2ba5376
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-15 20:03:30 +0530

    Solved 2P Problems

---
+++ TwoPointers/tripletSum.cpp
@@ -0,0 +1,25 @@
+// Link: https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1
+
+// Approach: Sorting + 2 Pointer + Binary Search [TC: O(N*N)]
+
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        sort(A,A+n); // Sorting
+        for(int i=0; i<n; i++){
+            // 2 Pointer - s,e
+            int s=i+1;
+            int e=n-1;
+            // Binary Search
+            while(s<e){
+                if(A[i]+A[s]+A[e]==X) return true;
+                if(A[i]+A[s]+A[e]<X) s++;
+                else e--;
+            }
+        }
+        return false;
+    }
+};
