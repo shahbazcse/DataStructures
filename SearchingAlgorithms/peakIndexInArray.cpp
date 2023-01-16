61d92c9050370f972aea10fc91cfd49c728a66a7
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-16 23:31:21 +0530

    Solved BinarySearch Problems

--- SearchingAlgorithms/peakIndexInArray.cpp
+++ SearchingAlgorithms/peakIndexInArray.cpp
@@ -9,10 +9,11 @@ public:
         int e=arr.size()-1;
         int mid=s+(e-s)/2;
         while(s<e){
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) break;
             if(arr[mid]<arr[mid+1]) s=mid+1;
             else e=mid;
             mid=s+(e-s)/2;
         }
-        return s;
+        return mid;
     }
 };
