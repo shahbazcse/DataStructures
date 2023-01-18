be3fdc4c5552bb64c213a25a035d60728e12922c
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-18 22:13:17 +0530

    Solved BinarySearch Problems

---
+++ SearchingAlgorithms/paintersPartition.cpp
@@ -0,0 +1,40 @@
+// Link: https://www.codingninjas.com/codestudio/problems/painter's-partition-problem_1089557
+
+// Approach: Binary Search [TC: O(LogN)]
+
+bool isPossible(vector<int> &boards, int k, int mid){
+    int painterCount=1;
+    int boardLength=0;
+    
+    for(int i=0; i<boards.size(); i++){
+        if(boardLength+boards[i]<=mid){
+            boardLength+=boards[i];
+        }
+        else{
+            painterCount++;
+            if(painterCount>k || boards[i]>mid) return false;
+            boardLength=boards[i];
+        }
+    }
+    return true;
+}
+
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    int s=0;
+    int sum=0;
+    for(int i=0; i<boards.size(); i++) sum+=boards[i];
+    int e=sum;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    
+    while(s<=e){
+        if(isPossible(boards,k,mid)){
+            ans=mid;
+            e=mid-1;
+        }
+        else s=mid+1;
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
