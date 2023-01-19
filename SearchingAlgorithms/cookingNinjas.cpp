a2020f518644a729477c45edc26091d40d57220f
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-19 21:54:57 +0530

    Solved BinarySearch Problems

--- SearchingAlgorithms/cookingNinjas.cpp
+++ SearchingAlgorithms/cookingNinjas.cpp
@@ -3,35 +3,41 @@
 // Approach: Binary Search [TC: O(N)]
 
 bool isPossible(vector<int> &rank, int m, int mid){
-    int count;
-    int dish=0;
-    int time;
+    int totalDish=0; // Total dish count
     
     for(int i=0; i<rank.size(); i++){
-        count=1;
-        time=0;
-        while(true){
-            if(time+(rank[i]*count)<=mid){
-                time+=rank[i]*count;
-                dish++;
-                count++;
-                if(dish==m) return true;
-            }else break;   
+        int chefDishCount=0; // ith chef dish count
+        int time=0; // time taken by ith chef
+        int j=1;
+        while(time+(rank[i]*j)<=mid){
+            time+=rank[i]*j;
+            chefDishCount++;
+            j++;
         }
+        totalDish+=chefDishCount;
     }
-    return false;
+    // Check if chefs were able to cook total dishes or not
+    if(totalDish>=m) return true;
+    else return false;
 }
 
 int minCookTime(vector<int> &rank, int m)
 {
-//     sort(rank.begin(),rank.end());
-    int s=0;
-    int maxi=INT_MIN;
-    for(int i=0; i<rank.size(); i++){
-        maxi=max(maxi,rank[i]);
-    }
-    int e=maxi*m*(m+1)/2;
+    sort(rank.begin(),rank.end());
+    
+    int s=0; // Starting time
+    
+    int totalTime=0;
+    
+    int slowestCook=rank[rank.size()-1];
+    
+    // Time taken by the slowest cook with maximum rank to cook all dishes will be the maximum time or end of search space
+    for(int i=1; i<=m; i++) totalTime+=slowestCook*i;
+    
+    int e=totalTime;
+    
     int mid=s+(e-s)/2;
+    
     int ans=-1;
     
     while(s<=e){
