6fbf1012bbda251ad09a510b33aac370e1629f2e
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-19 19:56:52 +0530

    Solved BinarySearch Problems

--- SearchingAlgorithms/aggressiveCows.cpp
+++ SearchingAlgorithms/aggressiveCows.cpp
@@ -15,46 +15,7 @@ bool isPossible(vector<int> &stalls, int k, int mid){
             }
             lastPosition=stalls[i];
         }
-    }#include <bits/stdc++.h>
-bool isPossible(vector<int> &stalls, int k, int mid){
-    int cowCount=1;
-    int lastPosition=stalls[0];
-    
-    for(int i=0; i<stalls.size(); i++){
-        if(stalls[i]-lastPosition>=mid){
-            cowCount++;
-            if(cowCount==k){
-                return true;
-            }
-            lastPosition=stalls[i];
-        }
     }
-    return false;
-}
-
-int aggressiveCows(vector<int> &stalls, int k)
-{
-
-    int s=0;
-    int maxi=INT_MIN;
-    int mini=INT_MAX;
-    for(int i=0; i<stalls.size(); i++){
-        maxi=max(maxi,stalls[i]);
-        mini=min(mini,stalls[i]);
-    }
-    int e=maxi-mini;
-    int mid=s-(e-s)/2;
-    int ans=-1;
-    
-    while(s<=e){
-        if(isPossible(stalls,k,mid)){
-            ans=mid;
-            s=mid+1;
-        }else e=mid-1;
-        mid=s+(e-s)/2;
-    }
-    return ans;
-}
     return false;
 }
 
