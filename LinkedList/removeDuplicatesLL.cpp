bbb36da1ef2d91534826ac09270317bd2d3da0e2
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-25 22:46:13 +0530

    Solved LinkedList Problems

---
+++ LinkedList/removeDuplicatesLL.cpp
@@ -0,0 +1,29 @@
+// Link: https://practice.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/1
+
+// Approach: [TC: O(N)]
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+        map<int, bool> found;
+        
+        Node* curr = head;
+        Node* prev = NULL;
+        
+        while(curr){
+            if(found[curr->data]){
+                prev->next=curr->next;
+                delete curr;
+                curr=prev;
+            }else{
+                found[curr->data]=true;
+                prev=curr;
+            }
+            curr=curr->next;
+        }
+        return head;
+    }
+};
