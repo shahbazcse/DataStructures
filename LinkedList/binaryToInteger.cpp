f50a93a7ff8f344e7ce81bb5cafb68afc253d9b2
shahbaz-cse <cseshahbaz@gmail.com>
2023-01-26 18:56:30 +0530

    Solved LinkedList Problems

--- LinkedList/binaryToInteger.cpp
+++ LinkedList/binaryToInteger.cpp
@@ -1,4 +1,7 @@
-// Problem: Convert Binary Number in a Linked List to Integer
+// Link: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+// Approach 1: [TC: O(N)]
+
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
@@ -7,6 +10,38 @@ public:
         while(temp){
             ans *= 2;
             ans += temp->val;
+            temp=temp->next;
+        }
+        return ans;
+    }
+};
+
+// Approach 2: Reverse LL [TC: O(N)]
+
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev= NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+
+        while(curr){
+            forward = curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+    int getDecimalValue(ListNode* head) {
+        ListNode* temp = reverse(head);
+        int ans=0;
+        int i=0;
+        while(temp){
+            if(temp->val!=0){
+                ans+=pow(2,i);
+            }
+            i++;
             temp=temp->next;
         }
         return ans;
