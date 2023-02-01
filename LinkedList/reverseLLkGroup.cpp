d0dab85f39f6c98b2530df3dd494494d879db750
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-01 17:21:38 +0530

    Solved LinkedList Problems

--- LinkedList/reverseLLkGroup.cpp
+++ LinkedList/reverseLLkGroup.cpp
@@ -32,3 +32,54 @@ public:
         return prev;
     }
 };
+
+// Link: https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+
+// Approach: Recursion [TC: O(!N)]
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        struct node* temp = head;
+        
+        int cnt=0;
+        while(temp){
+            temp=temp->next;
+            cnt++;
+        }
+        
+        if(cnt>=k){
+            struct node* prev = NULL;
+            struct node* curr = head;
+            struct node* forward = NULL;
+            
+            int i=0;
+            while(curr && i<k){
+                forward=curr->next;
+                curr->next=prev;
+                prev=curr;
+                curr=forward;
+                i++;
+            }
+            
+            head->next = reverse(forward,k);
+            head=prev;
+            return prev;
+            
+        }else{
+            struct node* prev = NULL;
+            struct node* curr = head;
+            struct node* forward = NULL;
+            
+            while(curr){
+                forward=curr->next;
+                curr->next=prev;
+                prev=curr;
+                curr=forward;
+            }
+            return prev;
+        }
+    }
+};
