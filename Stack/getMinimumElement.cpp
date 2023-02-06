5729a089223df009bd19aa8936e2deffb69a23da
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-07 03:36:04 +0530

    Solved Stack Problems

--- Stack/getMinimumElement.cpp
+++ Stack/getMinimumElement.cpp
@@ -45,3 +45,47 @@ class Solution{
             return;
        }
 };
+
+// Approach 2: [TC: O(1) / SC: O(1)]
+
+class Solution{
+    int minEle;
+    stack<int> s;
+    public:
+    
+       /*returns min element from stack*/
+       int getMin(){
+           if(s.empty()) return -1;
+           else return minEle;
+       }
+       
+       /*returns poped element from stack*/
+       int pop(){
+           if(s.empty()) return -1;
+           
+           int curr = s.top();
+           s.pop();
+           
+           if(curr<minEle){
+               int prevMin = minEle;
+               int val = 2*minEle - curr;
+               minEle = val;
+               return prevMin;
+           }else{
+               return curr;
+           }
+       }
+       
+       /*push element x into the stack*/
+       void push(int x){
+           if(s.empty()){
+               minEle = x;
+               s.push(x);
+           }
+           else if(x<minEle){
+               int val = 2*x - minEle;
+               s.push(val);
+               minEle = x;
+           }else s.push(x);
+       }
+};
