e5d7ebb1c4ddcac0bc36b846ed638c8a2b5d45a4
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-08 23:24:15 +0530

    Solved Queue Problems

--- Queue/NQueuesInArray.cpp
+++ Queue/NQueuesInArray.cpp
@@ -1,4 +1,73 @@
-// Link: 
+// Link: https://www.codingninjas.com/codestudio/problems/n-queue-using-array_1170053
 
 // Approach: 
 
+#include <bits/stdc++.h> 
+class NQueue{
+    int *arr;
+    int *front;
+    int *rear;
+    int *next;
+
+    int freeSpot;
+public:
+    // Initialize your data structure.
+    NQueue(int n, int s){
+        
+        front = new int[n];
+        rear = new int[n];
+
+        for(int i=0; i<n; i++){
+            front[i] = -1;
+            rear[i] = -1;
+        }
+        
+        arr = new int[s];
+        next = new int[s];
+
+        for(int i=0; i<s-1; i++){
+            next[i] = i+1;
+        }
+        next[s-1] = -1;
+
+        freeSpot = 0;
+    }
+
+    // Enqueues 'X' into the Mth queue. Returns true if it gets pushed into the queue, and false otherwise.
+    bool enqueue(int x, int m){
+        if(freeSpot == -1) return false;
+
+        int index = freeSpot;
+
+        freeSpot = next[index];
+
+        if(front[m-1] == -1){
+            front[m-1] = index;
+        }else{
+            next[rear[m-1]] = index;
+        }
+        
+        next[index] = -1;
+
+        rear[m-1] = index;
+        
+        arr[index] = x;
+        
+        return true;
+    }
+
+    // Dequeues top element from Mth queue. Returns -1 if the queue is empty, otherwise returns the popped element.
+    int dequeue(int m){
+        if(front[m-1] == -1) return -1;
+
+        int index = front[m-1];
+
+        front[m-1] = next[index];
+
+        next[index] = freeSpot;
+
+        freeSpot = index;
+
+        return arr[index];
+    }
+};
--- Queue/maxOfSubarraysOfKSize.cpp
+++ Queue/maxOfSubarraysOfKSize.cpp
@@ -1,4 +1,57 @@
-// Link: 
+// Link: https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1
 
-// Approach: 
+// Approach: [TC: O(N) / Aux. SC: O(K)]
 
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        deque<int> dq;
+        vector<int> ans;
+        
+        // check max in first k elements
+        for(int i=0; i<k; i++){
+            while(!dq.empty() && arr[i] > dq.front()){
+                dq.pop_front();
+            }
+            
+            while(!dq.empty() && arr[i] > dq.back()){
+                dq.pop_back();
+            }
+            
+            dq.push_back(arr[i]);
+        }
+        
+        // store max in k window
+        if(dq.size()!=0){
+            ans.push_back(dq.front());
+        }
+        
+        // check for remaining k windows of array
+        for(int i=k; i<n; i++){
+            // remove element from front
+            if(arr[i-k] == dq.front()) dq.pop_front();
+            
+            // remove element from front
+            while(!dq.empty() && arr[i] > dq.front()){
+                dq.pop_front();
+            }
+            
+            // remove element from back
+            while(!dq.empty() && arr[i] > dq.back()){
+                dq.pop_back();
+            }
+            
+            // store max in k window
+            dq.push_back(arr[i]);
+            
+            if(dq.size()!=0){
+                ans.push_back(dq.front());
+            }
+        }
+        // return list containing max elements in K sized windows in the array 
+        return ans;
+    }
+};
