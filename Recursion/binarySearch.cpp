4b620aea243ebd094cf10667c0649cd05a17119b
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-09 17:46:48 +0530

    Solved Recursion Problems

--- Recursion/binarySearch.cpp
+++ Recursion/binarySearch.cpp
@@ -1,4 +1,25 @@
-// Link: 
+// Link: https://practice.geeksforgeeks.org/problems/binary-search-1587115620/1
 
-// Approach: Recursion [TC: O() / SC: O()]
+// Approach: Recursion [TC: O(LogN) / SC: O(1)]
 
+class Solution {
+  public:
+    int helper(int arr[], int low, int high, int k){
+        // base case
+        if(low>high) return -1;
+        
+        int mid = low + (high - low)/2;
+        
+        if(arr[mid] == k) return mid;
+        
+        // recursive calls
+        if(arr[mid] < k) helper(arr,mid+1,high,k);
+        else helper(arr,low,mid-1,k);
+    }
+    
+    int binarysearch(int arr[], int n, int k) {
+        int low=0;
+        int high=n-1;
+        return helper(arr,low,high,k);
+    }
+};
