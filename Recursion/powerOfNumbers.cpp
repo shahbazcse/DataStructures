52d6907d0e15e2076822ea37050e0880b87fe6fd
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-09 21:58:44 +0530

    Solved Recursion Problems

--- Recursion/powerOfNumbers.cpp
+++ Recursion/powerOfNumbers.cpp
@@ -1,4 +1,19 @@
-// Link: 
+// Link: https://practice.geeksforgeeks.org/problems/power-of-numbers-1587115620/1
 
-// Approach: Recursion [TC: O() / SC: O()]
+// Approach: Recursion [TC: O(LogN) / SC: O(LogN)]
 
+class Solution{
+    public:
+    #define mod 1000000007
+    long long power(int N,int R)
+    {
+        if(R==0) return 1;
+        
+        long long ans = power(N,R/2);
+        ans = (ans*ans)%mod;
+        
+        if(R%2 == 0) return ans;
+        else return (ans*N)%mod;
+    }
+
+};
