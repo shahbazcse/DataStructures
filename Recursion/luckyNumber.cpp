bdf62dece82b1f1cf91bf62feb513c4dfbbfb37a
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-09 20:45:19 +0530

    Solved Recursion Problems

--- Recursion/luckyNumber.cpp
+++ Recursion/luckyNumber.cpp
@@ -1,4 +1,14 @@
-// Link: 
+// Link: https://practice.geeksforgeeks.org/problems/lucky-numbers2911/1
 
-// Approach: Recursion [TC: O() / SC: O()]
+// Approach: Recursion [TC: O(sqrt(N)) / SC: O(sqrt(N))]
 
+class Solution{
+public:
+    int count=1;
+    bool isLucky(int n) {
+        count++;
+        if(count>n) return true; // number is not deleted, hence lucky number
+        if(n%count==0) return false; // number is deleted, hence not a lucky number
+        return isLucky(n-(n/count));
+    }
+};
