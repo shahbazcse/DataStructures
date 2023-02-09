94565766794f0479961313de449cbb264235516a
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-09 14:17:04 +0530

    Solved Arrays Problems

--- Arrays/moveZeroes.cpp
+++ Arrays/moveZeroes.cpp
@@ -1,3 +1,24 @@
 // Link: https://leetcode.com/problems/move-zeroes/
 
-// Approach: 
+// Approach: [TC:: O(N) / SC: O(1)]
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        int j=0;
+
+        while(j+1<=nums.size()){
+            if(nums[i]==0 && nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+                j=i+1;
+            }else if(nums[i] == 0 && nums[j] == 0){
+                j++;
+            }else{
+                i++;
+                j++;
+            }
+        }
+    }
+};
