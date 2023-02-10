2d9d408d3b0dc00ceb13d60c0d80a70c9a02d396
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-10 18:00:32 +0530

    Solved Recursion Problems

--- Recursion/removeAdjacentDuplicates.cpp
+++ Recursion/removeAdjacentDuplicates.cpp
@@ -1,4 +1,34 @@
-// Link: 
+// Link: https://practice.geeksforgeeks.org/problems/recursively-remove-all-adjacent-duplicates0744/1
 
-// Approach: Recursion [TC: O() / SC: O()]
+// Approach: Recursion [TC: O(|S|) / SC: O(|S|)]
 
+class Solution{
+public:
+    string helper(string s){
+        string res;
+        int n=s.size();
+        int i=0;
+        
+        while(i<n){
+            if(i<n-1 && s[i]==s[i+1]){
+                while(i<n-1 && s[i]==s[i+1]){
+                    i++;
+                }
+            }else res.push_back(s[i]);
+            
+            i++;
+        }
+        return res;
+    }
+    
+    string rremove(string s){
+        string ans;
+        
+        while(s.size()!=ans.size()){
+            ans=s;
+            s=helper(ans);
+        }
+        
+        return ans;
+    }
+};
