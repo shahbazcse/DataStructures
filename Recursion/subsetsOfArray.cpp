7a9d2cf6108e96ced51ce37a2e0c217defb0088e
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-13 20:54:47 +0530

    Solved Recursion Problems

--- Recursion/subsetsOfArray.cpp
+++ Recursion/subsetsOfArray.cpp
@@ -1,6 +1,6 @@
 // Link: https://leetcode.com/problems/subsets/
 
-// Approach: [TC: O(2^N) / SC: O(N)]
+// Approach: Recursion [TC: O(2^N) / SC: O(N)]
 
 class Solution {
 public:
@@ -21,6 +21,37 @@ public:
         vector<int> ds;
         
         solve(nums,ans,ds,0);
+
+        return ans;
+    }
+};
+
+// Link: https://practice.geeksforgeeks.org/problems/subsets-1613027340/1
+
+// Approach: Recursion [TC: O(2^N) / SC: O(N)]
+class Solution
+{
+    public:
+    void solve(vector<int> A, vector<vector<int>> &ans, vector<int> &ds, int idx){
+        if(idx==A.size()){
+            ans.push_back(ds);
+            return;
+        }
+
+        ds.push_back(A[idx]);
+        solve(A,ans,ds,idx+1);
+        ds.pop_back();
+
+        solve(A,ans,ds,idx+1);
+    }
+    vector<vector<int> > subsets(vector<int>& A)
+    {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        
+        solve(A,ans,ds,0);
+        
+        sort(ans.begin(),ans.end()); // sort the subsets
 
         return ans;
     }
