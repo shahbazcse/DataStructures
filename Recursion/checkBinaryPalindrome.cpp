a299e8e97f7084dee52f2a8f84f13bcb1ff927d1
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-13 22:02:21 +0530

    Solved Recursion Problems

--- Recursion/checkBinaryPalindrome.cpp
+++ Recursion/checkBinaryPalindrome.cpp
@@ -1,4 +1,28 @@
-// Link: 
+// Link: https://practice.geeksforgeeks.org/problems/check-if-actual-binary-representation-of-a-number-is-palindrome0624/1
 
-// Approach: [TC: O() / SC: O()]
+// Approach: [TC: O(LogN) / SC: O(LogN)]
 
+class Solution{
+public:
+	void decToBin(long long int N, string &bin){
+	    if(N==0) return;
+	    
+	    bin += string(1,N%2);
+	    
+	    decToBin(N/2,bin);
+	}
+	
+	int binaryPalin (long long int N)
+	{
+	    string bin;
+	    
+	    decToBin(N,bin);
+	    
+	    string rev = bin;
+	    
+	    reverse(rev.begin(),rev.end());
+	    
+	    if(rev == bin) return 1;
+	    else return 0;
+	}
+};
