15ebe964c4db74adc9ff1c02bf07f14ba634bcdf
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-13 20:49:02 +0530

    Solved Recursion Problems

--- Recursion/subsetsOfArray.cpp
+++ Recursion/subsetsOfArray.cpp
@@ -1,4 +1,27 @@
 // Link: https://leetcode.com/problems/subsets/
 
-// Approach: [TC: O() / SC: O()]
+// Approach: [TC: O(2^N) / SC: O(N)]
 
+class Solution {
+public:
+    void solve(vector<int> nums, vector<vector<int>> &ans, vector<int> &ds, int idx){
+        if(idx==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+
+        ds.push_back(nums[idx]);
+        solve(nums,ans,ds,idx+1);
+        ds.pop_back();
+
+        solve(nums,ans,ds,idx+1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        
+        solve(nums,ans,ds,0);
+
+        return ans;
+    }
+};
