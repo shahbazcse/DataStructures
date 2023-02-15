d5b746f2df22448cedcd09fd2df53b28d424b62f
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-15 17:56:25 +0530

    Solved Recursion Problems

--- Recursion/combinationSum1.cpp
+++ Recursion/combinationSum1.cpp
@@ -32,3 +32,31 @@ public:
         return ans;
     }
 };
+
+// Link: https://practice.geeksforgeeks.org/problems/combination-sum-1587115620/1
+
+// Approach: [TC: O((2^Target)*K) / SC: O(K*X)]
+// K is length of ans, X is number of combinations
+
+class Solution {
+  public:
+    void solve(vector<int> A, int B, vector<vector<int>> &ans, vector<int> &ds, int idx){
+        if(idx==A.size()){
+            if(B==0) ans.push_back(ds);
+            return;
+        }
+        if(A[idx]<=B && A[idx]!=A[idx-1]){
+            ds.push_back(A[idx]);
+            solve(A,B-A[idx],ans,ds,idx);
+            ds.pop_back();
+        }
+        solve(A,B,ans,ds,idx+1);
+    }
+    vector<vector<int> > combinationSum(vector<int> &A, int B) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(A.begin(),A.end());
+        solve(A,B,ans,ds,0);
+        return ans;
+    }
+};
