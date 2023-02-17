620f51d60ed4676bdc5259c55597648477da46fe
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-18 00:02:03 +0530

    Solved Recursion Problems

--- Recursion/permutationSequence.cpp
+++ Recursion/permutationSequence.cpp
@@ -10,7 +10,7 @@ public:
         int fact = 1;
         vector<int> numbers;
 
-        // store all numbers in a list and calculate factorial
+        // store all numbers in a list and calculate factorial (n-1)!
         for(int i=1; i<n; i++){
             fact = fact * i;
             numbers.push_back(i);
@@ -22,12 +22,18 @@ public:
         k--; // reduce by 1 for 0-based indexing
 
         while(1){
+            // add the first element of required permutation
             ans += to_string(numbers[k/fact]);
+            // erase that element from vector
             numbers.erase(numbers.begin() + k/fact);
 
+            // break loop when numbers list is empty and we got our kth sequence
             if(numbers.size()==0) break;
 
+            // find the position for remaining elements
             k = k % fact;
+            
+            // find the required factorial
             fact = fact / numbers.size();
         }
         return ans;
