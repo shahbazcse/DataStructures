8fa825fa768172b862b81c53e0a3b59454ae52a8
shahbaz-cse <cseshahbaz@gmail.com>
2023-02-19 23:11:18 +0530

    Solved Recursion Problems

--- Recursion/generateBinaryStrings.cpp
+++ Recursion/generateBinaryStrings.cpp
@@ -6,13 +6,17 @@ class Solution{
 public:
     void solve(int num, string ds, int idx){
         if(idx==num){
+        // print after generating a unique string
             cout<<ds<<" ";
             return;
         }
         
+        // add 0 to string
         solve(num,ds+'0',idx+1);
         
+        // skip if 1 is present at the end of string to avoid consecutive 1s
         if(ds.back()!='1'){
+            // add 1 to string
             solve(num,ds+'1',idx+1);
         }
     }
