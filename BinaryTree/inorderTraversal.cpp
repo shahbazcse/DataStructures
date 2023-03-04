6cef351211c8a53c3dae27428dfb645f2a2e12c5
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-04 17:26:26 +0530

    Solved BinaryTree Problems

--- BinaryTree/inorderTraversal.cpp
+++ BinaryTree/inorderTraversal.cpp
@@ -1,6 +1,6 @@
 // Link: https://leetcode.com/problems/binary-tree-inorder-traversal
 
-// Approach: Recursion [TC: O(N) / SC: O(N)]
+// Approach 1: Recursion [TC: O(N) / SC: O(N)]
 
 /**
  * Definition for a binary tree node.
@@ -33,6 +33,42 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
         solve(root,ans);
+        return ans;
+    }
+};
+
+// Approach 2: Iterative + Stack [TC: O(N) / SC: O(N)]
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            TreeNode* top = st.top();
+            st.pop();
+
+            ans.push_back(top->val);
+
+            if(top->right!=NULL) st.push(top->right);
+            if(top->left!=NULL) st.push(top->left);
+
+        }
         return ans;
     }
 };
