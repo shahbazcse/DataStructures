94b7b911a35a8735309d74b95b57341888e7d352
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-07 18:14:50 +0530

    Solved BinaryTree Problems

---
+++ BinaryTree/topViewBinaryTree.cpp
@@ -0,0 +1,43 @@
+// Link: https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1
+
+// Approach: Iterative + Map + Queue [TC: O(NLogN) / SC: O(N)]
+
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        
+        map<int,int> mpp;
+        
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            
+            Node* node = it.first;
+            
+            int col = it.second;
+            
+            if(mpp.find(col)==mpp.end()){
+                mpp[col] = node->data;
+            }
+            
+            if(node->left) q.push({node->left,col-1});
+            if(node->right) q.push({node->right,col+1});
+        }
+        
+        for(auto it : mpp){
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+
+};
