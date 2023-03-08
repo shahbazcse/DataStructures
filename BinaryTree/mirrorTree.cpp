80aaf62e20edaa42281f64de8b1e1d754acf38ec
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-08 22:39:10 +0530

    Solved BinaryTree Problems

---
+++ BinaryTree/mirrorTree.cpp
@@ -0,0 +1,17 @@
+// Link: https://practice.geeksforgeeks.org/problems/mirror-tree/1
+
+// Apporoach: [TC: O(N) / SC: O(N)]
+
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        if(node==NULL) return;
+        
+        mirror(node->left);
+        mirror(node->right);
+
+        // if either of the child nodes exist, swap both the child nodes with data and address
+        if(node->left || node->right) swap(node->left,node->right);
+    }
+};
