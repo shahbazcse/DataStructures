21ae558ecb7bcfb2abea896f4c08971c27bd7334
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-11 23:51:32 +0530

    Solved BinaryTree Problems

---
+++ BinaryTree/morrisInorderTraversal.cpp
@@ -0,0 +1,51 @@
+// Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+// Approach: Morris Inorder Traversal [TC: O(N) / SC: O(1)]
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // Create an empty vector to store the inorder traversal
+        vector<int> inorder;
+        
+        // Start from the root of the tree
+        TreeNode* curr = root;
+
+        while(curr){
+            // If the current node has no left child, then it is a leaf node, store it and move to its right child (if any).
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }else{
+                // If the current node has a left child, then we need to find its 
+                // in-order predecessor (i.e., the largest node in its left subtree).
+                // To do this, we start from the left child and follow its right child
+                // pointers until we reach a node that either has no right child or 
+                // whose right child is the current node (Threading).
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev = prev->right;
+                }
+
+                if(prev->right==NULL){
+                    // If we reach a node with no right child, we link it to the current node
+                    // (by setting its right child to point to the current node, aka Threading) and move to 
+                    // the current node's left child.
+                    prev->right = curr;
+                    curr = curr->left;
+                }else{
+                    // If we reach a node whose right child is the current node (Threading is already done), then we 
+                    // have already visited its left subtree, so we unlink it (by setting 
+                    // its right child to NULL), store the current node, and move to its 
+                    // right child (if any).
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        
+        // Return the inorder traversal
+        return inorder;
+    }
+};
---
+++ BinaryTree/morrisPreorderTraversal.cpp
@@ -0,0 +1,4 @@
+// Link: https://practice.geeksforgeeks.org/problems/preorder-traversal-iterative/1
+
+// Approach: Morris Preorder Traversal [TC: O(N) / SC: O(1)]
+
