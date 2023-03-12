298cb6ab54300f1ee3e934fef2b2085f6ab4b30d
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-12 21:43:21 +0530

    Solved Arrays Problems

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -1,5 +1,6 @@
 // Link: https://leetcode.com/problems/two-sum/
 
+// Approach 1: [TC: O(N^2) / SC: O(1)]
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -14,5 +15,32 @@ public:
             }
         }
         return ans;
+    }
+};
+
+// Approach 2: Map [TC: O(N) / SC: O(N)]
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int first = -1;
+        int last = -1;
+
+        unordered_map<int,int> mpp;
+
+        for(int i=0; i<nums.size(); i++){
+            first = i;
+            int key = target - nums[i];
+
+            auto itr = mpp.find(key);
+            
+            if(itr!=mpp.end()){
+                last = itr->second;
+                break;
+            }
+            mpp[nums[i]] = i;
+        }
+
+        return {first,last};
     }
 };
