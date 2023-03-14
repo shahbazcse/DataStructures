6f5bb68f1d069a00723d2e96e17b822a7fe0121a
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-14 16:50:15 +0530

    Solved BinaryTree Problems

--- LS/BinaryTree/iterativePostorder1Stack.cpp
+++ LS/BinaryTree/iterativePostorder1Stack.cpp
@@ -34,3 +34,76 @@ public:
         return ans; // return ans vector containing postorder traversal
     }
 };
+
+// Approach 2: Iterative + 1 Stack [TC: O(N) / SC: O(N)]
+
+/*
+    Intuition: Push directly root node two times while traversing to the left. 
+              While popping if you find stack top() is same as root 
+              then go for root->right else print root.
+*/
+
+class Solution{
+    public:
+    vector<int> postOrder(Node* node) {
+        Node* curr = node;
+        
+        vector<int> postorder;
+        
+        if(curr==NULL) return postorder;
+        
+        stack<Node*> st;
+        
+        while(1){
+            while(curr){
+                st.push(curr);
+                st.push(curr);
+                curr = curr->left;
+            }
+            
+            if(st.empty()) break;
+            
+            curr = st.top();
+            st.pop();
+            
+            if(!st.empty() && st.top() == curr) curr = curr->right;
+            else{
+                postorder.push_back(curr->data);
+                curr = NULL;
+            }
+        }
+        return postorder;
+    }
+};
+
+// Approach 3: Iterative + 1 Stack + Hashing [TC: O(N) / SC: O(N)]
+
+class Solution{
+    public:
+    vector<int> postOrder(Node* node) {
+        vector<int> postorder;
+        
+        if(node==NULL) return postorder;
+        
+        unordered_map<Node*,int> visited;
+        
+        stack<Node*> st;
+        st.push(node);
+        
+        while(!st.empty()){
+            Node* curr = st.top();
+            
+            visited[curr] = 1;
+            
+            if(curr->left && !visited[curr->left]){
+                st.push(curr->left);
+            }else if(curr->right && !visited[curr->right]){
+                st.push(curr->right);
+            }else{
+                postorder.push_back(curr->data);
+                st.pop();
+            }
+        }
+        return postorder;
+    }
+};
