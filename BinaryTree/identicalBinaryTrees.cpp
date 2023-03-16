86429183e35421f561d17af4b21fa53d56d9f889
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-16 11:07:54 +0530

    Solved BinaryTree Problems

--- BinaryTree/identicalBinaryTrees.cpp
+++ BinaryTree/identicalBinaryTrees.cpp
@@ -5,14 +5,13 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // if either of them is NULL, the other must be NULL too if both trees are identical
+        // if either of them is NULL, the other must be NULL too if both trees are identical, hence return true
+        // if either of them is not NULL, then they are not identical, return false
         if(p==NULL || q==NULL){
-            if(p==q) return true; // return true, if both nodes are NULL
-            else return false; // return false, if either of them is not NULL
+            return p==q;
         }
 
         // check if values of both current nodes are equal and make recursive calls to check their child nodes, if they all are identical, return true
-        if(p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right)) return true;
-        else return false; // return false, if the current nodes are unequal or their child nodes are not identical
+        return (p->val == q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
---
+++ LS/BinaryTree/findDuplicateSubtreesInBT.cpp
@@ -0,0 +1,3 @@
+// Link: https://leetcode.com/problems/find-duplicate-subtrees/
+
+// Approach 1: [TC: O() / SC: O()]
