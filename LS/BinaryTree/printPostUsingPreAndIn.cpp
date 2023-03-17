f0f67d75c3254abfa390b4d1222a139657b3430e
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-17 18:23:08 +0530

    Solved BinaryTree Problems

--- LS/BinaryTree/printPostUsingPreAndIn.cpp
+++ LS/BinaryTree/printPostUsingPreAndIn.cpp
@@ -29,3 +29,33 @@ void printPostOrder(int in[], int pre[], int n)
 
 // Approach 2: Recursion + Map [TC: O(N) / SC: O(N)]
 
+void print(int pre[], int &preStart, int in[], int inStart, int inEnd, unordered_map<int,int> &inMap){
+    if(inStart > inEnd) return;
+
+    // Starting node of preorder will be root, use node value to search on map and find root index in inorder
+    int rootIdx = inMap[pre[preStart++]]; // increment preStart by 1 to get next root node in next recursive call
+
+    // make recursive call for left subtree
+    print(pre,preStart,in,inStart,rootIdx-1,inMap);
+
+    // make recursive call for right subtree
+    print(pre,preStart,in,rootIdx+1,inEnd,inMap);
+
+    // print root node at the end (postorder traversal)
+    cout<<in[rootIdx]<<" ";
+}
+
+void printPostOrder(int in[], int pre[], int n)
+{
+    // mapping inorder traversal
+    unordered_map<int,int> inMap;
+
+    for(int i=0; i<n; i++){
+        inMap[in[i]] = i;
+    }
+
+    // starting of preorder will always have root node of each subtree
+    int preStart = 0;
+
+    print(pre, preStart, in, 0, n-1, inMap);
+}
