aad3a3226f0236237e34b3cd9b8cf7827c4842eb
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-18 14:40:21 +0530

    Solved BinarySearchTree Problems

---
+++ BinarySearchTree/floorOfBST.cpp
@@ -0,0 +1,23 @@
+// Link: https://practice.geeksforgeeks.org/problems/floor-in-bst/1
+
+// Approach: Iterative [TC: O(N) / SC: O(1)]
+
+int floor(Node* root, int x) {
+    if(root==NULL) return -1;
+    
+    int floor = -1;
+    
+    while(root){
+        if(root->data == x){
+            floor = root->data;
+            break;
+        }
+        
+        if(root->data > x) root = root->left;
+        else{
+            floor = root->data;
+            root = root->right;
+        }
+    }
+    return floor;
+}
