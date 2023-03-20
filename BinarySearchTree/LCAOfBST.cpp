66a8fdce4ef23d10e770773598142cb695973b80
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-20 18:07:42 +0530

    Solved BinarySearchTree Problems

--- BinarySearchTree/LCAOfBST.cpp
+++ BinarySearchTree/LCAOfBST.cpp
@@ -19,3 +19,24 @@ public:
         return root;
     }
 };
+
+// Approach 2: Iterative [TC: O(Height) / SC: O(1)]
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* curr = root;
+
+        while(curr){
+            // Both are on left
+            if(root->val > p->val && root->val > q->val){
+                root=root->left;
+            } // Both are on right
+            else if(root->val < p->val && root->val < q->val){
+                root=root->right;
+            } // if both are not on the either side of current node, then current node is the point of intersection or LCA of p and q, so return current node
+            else return root;
+        }
+        return NULL;
+    }
+};
