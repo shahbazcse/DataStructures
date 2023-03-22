2e45cd385e25cbc91b2748400a34b480c9bf0aed
shahbaz-cse <cseshahbaz@gmail.com>
2023-03-22 22:48:16 +0530

    Solved BinarySearchTree Problems

--- BinarySearchTree/largestBST.cpp
+++ BinarySearchTree/largestBST.cpp
@@ -36,3 +36,51 @@ class Solution{
     	return maxi;
     }
 };
+
+// Approach 2: Recursion + Min/Max [TC: O(N) / SC: O(N)]
+
+// A struct to store the properties of a node in a binary tree
+struct NodeValue{
+    int mini;    // Minimum value in the subtree rooted at this node
+    int maxi;    // Maximum value in the subtree rooted at this node
+    bool isBST;  // Whether the subtree rooted at this node is a valid BST or not
+    int size;    // Size of the largest BST in the subtree rooted at this node
+};
+
+// A recursive helper function to calculate the largest BST in a binary tree
+NodeValue helper(Node* root, int &ans){
+    // If the current node is NULL, return a NodeValue with default values
+    if(root==NULL) return {INT_MAX,INT_MIN,true,0};
+        
+    // Recursively calculate the properties of the left and right subtrees
+    NodeValue left = helper(root->left,ans);
+    NodeValue right = helper(root->right,ans);
+        
+    // Calculate the properties of the current node based on the properties of its left and right subtrees
+    NodeValue currentNode;
+        
+    currentNode.size = left.size + right.size + 1;  // Size of the subtree rooted at this node
+        
+    currentNode.mini = min(root->data,left.mini);  // Minimum value in the subtree rooted at this node
+    currentNode.maxi = max(root->data,right.maxi);  // Maximum value in the subtree rooted at this node
+        
+    // Check if the subtree rooted at this node is a valid BST
+    currentNode.isBST = ((root->data > left.maxi && root->data < right.mini) && left.isBST && right.isBST);
+        
+    // If the subtree rooted at this node is a valid BST, update the answer with its size
+    if(currentNode.isBST) ans = max(ans,currentNode.size);
+        
+    // Return the properties of the subtree rooted at this node
+    return currentNode;
+}
+    
+// A class that contains the function to calculate the largest BST in a binary tree
+class Solution{
+    public:
+    int largestBst(Node *root)
+    {
+        int ans = 0;
+        NodeValue temp = helper(root,ans);
+        return ans;
+    }
+};
