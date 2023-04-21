d5f0d271647840342222347ef961a191bd8fc249
shahbaz-cse <cseshahbaz@gmail.com>
2023-04-21 20:50:08 +0530

    Solved Graph Problems

--- Graph/detectCycleInUndirectedGraph.cpp
+++ Graph/detectCycleInUndirectedGraph.cpp
@@ -47,3 +47,41 @@ class Solution {
 
 // Approach 2: DFS Traversal [TC: O() / SC: O()]
 
+class Solution {
+  public:
+    bool detectUsingDFS(int src, int parent, vector<int> adj[], int vis[]){
+        vis[src] = 1;
+        
+        for(auto node : adj[src]){
+            if(!vis[node]){
+                // if a cyclic graph component is found in the recursive call, return true
+                if(detectUsingDFS(node,src,adj,vis)){
+                    return true;
+                }
+            }
+            /*  if the 'node' is already visited and it is not the parent, 
+                then that 'node' must be visited by some other node in the recursion tree before, 
+                which means the graph is cyclic.
+            */
+            else if(node!=parent){
+                return true;
+            }
+        }
+        // if no cyclic graph component is found in the recursive call, return false
+        return false;
+    }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        int vis[V] = {0};
+        
+        // checking for all connected components
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(detectUsingDFS(i,-1,adj,vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
