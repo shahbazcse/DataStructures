312b9fb6ead13bd5910c8f73bee86e2662e033e4
shahbaz-cse <cseshahbaz@gmail.com>
2023-05-01 20:57:47 +0530

    Solved Graph Problems

--- Graph/bipartiteGraph.cpp
+++ Graph/bipartiteGraph.cpp
@@ -1,4 +1,41 @@
 // Link: https://practice.geeksforgeeks.org/problems/bipartite-graph/1
 
-// Approach: [TC: O() / SC: O()]
+// Approach 1: BFS + Hashing [TC: O(V+2E) / SC: O(V)]
 
+class Solution {
+public:
+    bool check(int start, int V, vector<int> adj[], int color[]){
+	    color[start] = 0;
+	    
+	    queue<int> q;
+	    q.push(start);
+	    
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        
+	        for(auto it : adj[node]){
+	            if(color[it] == -1){
+	                color[it] = !color[node];
+	                q.push(it);
+	            }
+	            else if(color[it] == color[node]){
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+    }
+	bool isBipartite(int V, vector<int>adj[]){
+        int color[V];
+	    for(int i=0; i<V; i++) color[i]=-1;
+	    
+        // check for all connected components
+	    for(int i=0; i<V; i++){
+	        if(color[i] == -1){
+	            if(check(i,V,adj,color) == false) return false;
+	        }
+	    }
+	    return true;
+	}
+};
