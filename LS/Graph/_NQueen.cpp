8a0185b333e9110ebc55e27d8d4b51d28f976a8c
shahbaz-cse <cseshahbaz@gmail.com>
2023-05-24 22:23:22 +0530

    Solved Graph Problems

---
+++ LS/Graph/_NQueen.cpp
@@ -0,0 +1,4 @@
+// Link: https://practice.geeksforgeeks.org/problems/n-queen-problem0315/1
+
+// Approach: [TC: O() / SC: O()]
+
---
+++ LS/Graph/_knightWalk.cpp
@@ -0,0 +1,4 @@
+// Link: https://practice.geeksforgeeks.org/problems/knight-walk4521/1
+
+// Approach: [TC: O() / SC: O()]
+
