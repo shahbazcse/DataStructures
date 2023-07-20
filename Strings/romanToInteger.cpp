698503f9dba43b61eb58b8136f6fce6f20333a0a
shahbazcse <cseshahbaz@gmail.com>
2023-07-20 21:56:54 +0530

    Solved Strings Problem

--- Strings/romanToInteger.cpp
+++ Strings/romanToInteger.cpp
@@ -1,4 +1,33 @@
-// Link: 
+// Link: https://leetcode.com/problems/roman-to-integer/
 
-// Approach: [TC: O() / SC: O()]
+// Approach: Map [TC: O(|S|) / SC: O(|S|)]
 
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> roman;
+
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+
+
+        int ans = 0;
+
+        int curr = 0;
+        while(curr != s.length()){
+            if(roman[s[curr]] < roman[s[curr+1]]){
+                ans -= roman[s[curr]];
+            }else{
+                ans += roman[s[curr]];
+            }
+            curr++;
+        }
+
+        return ans;
+    }
+};
