627177de119a7ddfba597229bdbe5d46e18d130b
shahbazcse <cseshahbaz@gmail.com>
2023-08-02 21:41:49 +0530

    Solved Strings Problem

--- Strings/countAndSay.cpp
+++ Strings/countAndSay.cpp
@@ -1,4 +1,28 @@
 // Link: https://leetcode.com/problems/count-and-say/
 
-// Approach: [TC: O() / SC: O()]
+// Approach: Recursion [TC: O(2^N) / SC: O(2^N)]
 
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 0) return "";
+        if(n == 1) return "1";
+
+        string str = countAndSay(n-1);
+
+        if(str.size() == 1) return to_string(1)+str;
+
+        string temp = "";
+        int i=0;
+        int j=str.size();
+        while(i < str.size()){
+            j = i;
+            while(i<str.size() && str[i] == str[j]){
+                i++;
+            }
+            temp += to_string(i-j) + str[j];
+        }
+        
+        return temp;
+    }
+};
